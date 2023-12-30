428f3fe82870172a4082f702e9d733bb2c2bb063
Jair1810 <jair18101@hotmail.com>
2023-12-30 12:11:38 -0600

    Ya se pueden agregar tareas (mediante variables, no se guarda localmente)

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, TextInput, TouchableOpacity, FlatList} from 'react-native';
 import TaskItem from './src/components/TaskItem';
 import styles from './src/styles';
+import {Task} from './src/types';
 
-const tasks = [
+/*const tasks = [
   {
     title: 'Alimentar al perro.',
     done: false,
@@ -19,7 +20,7 @@ const tasks = [
     done: true,
     date: new Date(),
   },
-];
+];*/
 
 const deleteFunction = () => {
   console.log('Deleteado');
@@ -31,6 +32,21 @@ const markDone = () => {
 //Se tipa con una interfaz para que se puedan obtener de manera correcta los elementos.
 
 export default function App() {
+  const [task, setTask] = useState<Task[]>([]);
+  const [text, setText] = useState<string>('');
+
+  const addTask = () => {
+    const tmp = [...task];
+    const newTask = {
+      title: text,
+      done: false,
+      date: new Date(),
+    };
+
+    tmp.push(newTask);
+    setTask(tmp);
+    setText('');
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Mis tareas por hacer</Text>
@@ -39,10 +55,12 @@ export default function App() {
           <TextInput
             style={styles.textInput}
             placeholder="Agregar una nueva tarea"
+            value={text}
+            onChangeText={(t: string) => setText(t)}
           />
         </View>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity style={styles.button} onPress={addTask}>
             <Text style={styles.whiteText}>Agregar</Text>
           </TouchableOpacity>
         </View>
@@ -50,6 +68,7 @@ export default function App() {
       <View style={styles.scrollcontainer}>
         {/* Se pasan los elementos a la función que se hace cargo de proyectarlos y en data se pasan los elementos. */}
         <FlatList
+          data={task}
           renderItem={({item}) => (
             <TaskItem
               item={item}
@@ -57,7 +76,6 @@ export default function App() {
               markDone={markDone}
             />
           )}
-          data={tasks}
         />
       </View>
     </View>
