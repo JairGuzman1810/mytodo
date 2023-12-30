9d738c345e413d66ab0fe88f1a2119bfa1e5831c
Jair1810 <jair18101@hotmail.com>
2023-12-30 11:21:12 -0600

    Refactorización de styles. Agregado inicial del marcar la tarea y eliminar.

--- App.tsx
+++ App.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  FlatList,
-} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity, FlatList} from 'react-native';
 import TaskItem from './src/components/TaskItem';
-import {TaskItemProps} from './src/components/interfaces';
+import styles from './src/styles';
 
 const tasks = [
   {
@@ -27,11 +20,15 @@ const tasks = [
     date: new Date(),
   },
 ];
+
+const deleteFunction = () => {
+  console.log('Deleteado');
+};
+const markDone = () => {
+  console.log('marqueado.');
+};
 // Pasamos todos nuestros elementos de task para que se visualice,
 //Se tipa con una interfaz para que se puedan obtener de manera correcta los elementos.
-const renderItem = ({item}: TaskItemProps) => {
-  return <TaskItem item={item} />;
-};
 
 export default function App() {
   return (
@@ -52,55 +49,17 @@ export default function App() {
       </View>
       <View style={styles.scrollcontainer}>
         {/* Se pasan los elementos a la función que se hace cargo de proyectarlos y en data se pasan los elementos. */}
-        <FlatList renderItem={renderItem} data={tasks} />
+        <FlatList
+          renderItem={({item}) => (
+            <TaskItem
+              item={item}
+              deleteFunction={deleteFunction}
+              markDone={markDone}
+            />
+          )}
+          data={tasks}
+        />
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    borderWidth: 1,
-    flex: 1,
-  },
-  title: {
-    fontSize: 20,
-    color: '#6f6f6f',
-  },
-  text: {
-    fontSize: 14,
-    color: '#6f6f6f',
-  },
-  whiteText: {
-    fontSize: 14,
-    color: '#ffff',
-  },
-  textInput: {
-    borderColor: '#6f6f6f',
-    borderWidth: 1,
-    borderRadius: 16,
-    paddingLeft: 15,
-  },
-  inputContainer: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  buttonContainer: {
-    justifyContent: 'center',
-  },
-  content: {
-    marginTop: 20,
-    flexDirection: 'row',
-  },
-  button: {
-    backgroundColor: '#5897df',
-    padding: 12,
-    alignItems: 'center',
-    borderRadius: 10,
-    marginLeft: 20,
-  },
-  scrollcontainer: {
-    marginTop: 20,
-  },
-});
